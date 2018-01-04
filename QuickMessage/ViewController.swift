38600b7d729315d08f8f2117ba0ce375e0b65805
Rachael Little <realrachael@gmail.com>
2018-01-04 13:29:59 -0500

    Initial Commit

---
+++ QuickMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickMessage
+//
+//  Created by Rachael Little on 1/4/18.
+//  Copyright © 2018 Rachael Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuickMessageTests/QuickMessageTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuickMessageTests.swift
+//  QuickMessageTests
+//
+//  Created by Rachael Little on 1/4/18.
+//  Copyright © 2018 Rachael Little. All rights reserved.
+//
+
+import XCTest
+@testable import QuickMessage
+
+class QuickMessageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
